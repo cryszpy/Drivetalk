3515d44d555b9b2fbd94d504879e1f734aa902ad
Chris Huang <huang.chrisf@gmail.com>
2024-10-13 15:37:15 -0700

    + Added taxi stops and driving to nearest one - Reconfigured Car object hierarchy

--- Drivetalk/Assets/Scripts/CarController.cs
+++ Drivetalk/Assets/Scripts/CarController.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class CarController : MonoBehaviour
 {
     [Header("SCRIPT REFERENCES")]
+
+    public List<GameObject> taxiStops = new();
 
     [SerializeField] private GameObject destination;
 
@@ -13,9 +18,26 @@ public class CarController : MonoBehaviour
 
     public NavMeshAgent agent;
 
+    public bool arrived;
+
     private void Update() {
-        if (Input.GetMouseButtonDown(1) && destination) {
-            agent.SetDestination(destination.transform.position);
+        if (arrived) {
+            FindNearestStop();
         }
+    }
+
+    private void FindNearestStop() {
+        List<float> stopDistances = new();
+
+        foreach (var stop in taxiStops) {
+            float distance = Vector3.Distance(stop.transform.position, rb.position);
+            stopDistances.Add(distance);
+        }
+        
+
+        if (stopDistances.Count != 0) {
+            agent.SetDestination(taxiStops[stopDistances.IndexOf(stopDistances.Min())].transform.position);
+        }
+        arrived = false;
     }
 }
--- Drivetalk/Assets/Scripts/Destination.cs
+++ Drivetalk/Assets/Scripts/Destination.cs
@@ -5,6 +5,12 @@ public class Destination : MonoBehaviour
     private void OnTriggerEnter(Collider collider) {
         if (collider.CompareTag("Car")) {
             Debug.Log("Arrived at destination!");
+
+            // PUT TRIP SUMMARY CODE HERE? MAYBE?
+
+            if (collider.TryGetComponent<CarController>(out var script)) {
+                script.arrived = true; // TODO: only set this when trip summary is over and it's time to get back to driving
+            }
         }
     }
 }
